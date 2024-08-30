e97fc7d49332a0b52da2369eccba65a1094507a3
Atiar-Rahman <atiar1709@gmail.com>
2024-08-30 10:43:47 +0600

    firebase setup and googleSinein and signout

--- src/Pages/Home.jsx/Home.jsx
+++ src/Pages/Home.jsx/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
             this is home
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import app from '../../firebase/firebase.init';
 
 const Login = () => {
+
+    const [user,setUser] = useState();
+   const auth = getAuth(app);
+   const googleProvider = new GoogleAuthProvider();
+
+   const handleGoogleSignIN = () => {
+    signInWithPopup(auth,googleProvider)
+    .then(result=>setUser(result.user))
+    .catch(error=>console.log(error))
+   }
+   const handleSignOut = () =>{
+    signOut(auth)
+    .then()
+    .catch(error=> console.error(error))
+   }
     return (
         <div>
-            <h2>this is login page</h2>
+            {user ?
+            <button className='btn btn-secondary btn-outline' onClick={handleSignOut}>Sign Out</button>:
+            <button className='btn btn-primary btn-outline' onClick={handleGoogleSignIN}>Google SignIn</button>}
+            
         </div>
     );
 };
